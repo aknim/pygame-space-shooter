e428d9da3ffd43c6ede69f06fd7c4faa01836faa
akshaya <akshayasunny@gmail.com>
2024-11-20 18:44:45 +0530

    Day 1.2 Added bullets firing

--- space_shooter.py
+++ space_shooter.py
@@ -18,6 +18,10 @@ player_x = (WIDTH - player_width) // 2
 player_y = HEIGHT - 60
 player_speed = 5
 
+bullet_width, bullet_height = 5, 10
+bullet_speed = 7
+bullets = []
+
 running = True
 while running:
     screen.fill(BLACK)
@@ -26,11 +30,27 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bullet_x = player_x + player_width // 2 - bullet_width // 2
+                bullet_y = player_y
+                bullets.append(pygame.Rect(bullet_x, bullet_y, bullet_width, bullet_height))
+
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and player_x > 0:
         player_x -= player_speed
     if keys[pygame.K_RIGHT] and player_x < WIDTH - player_width:
         player_x += player_speed
+
+    # Update bullet positions
+    for bullet in bullets[:]:
+        bullet.y -= bullet_speed
+        if bullet.y < 0:
+            bullets.remove(bullet)
+
+    for bullet in bullets:
+        pygame.draw.rect(screen, WHITE, bullet)
 
     pygame.draw.rect(screen, WHITE, (player_x, player_y, player_width, player_height))
 
